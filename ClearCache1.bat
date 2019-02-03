4fbcb0ed0d3dedae46b82000edec1053aa2b87a6
Venkatesh G <33002659+VenkateshGopal@users.noreply.github.com>
2019-02-03 11:28:23 +0530

    Add files via upload

---
+++ ClearCache1.bat
@@ -0,0 +1,18 @@
+taskkill /F /IM iedriverserver.exe
+
+erase "%TEMP%\*.*" /f /s /q
+for /D %%i in ("%TEMP%\*") do RD /S /Q "%%i"
+
+erase "%TMP%\*.*" /f /s /q
+for /D %%i in ("%TMP%\*") do RD /S /Q "%%i"
+
+erase "%ALLUSERSPROFILE%\TEMP\*.*" /f /s /q
+for /D %%i in ("%ALLUSERSPROFILE%\TEMP\*") do RD /S /Q "%%i"
+
+erase "%SystemRoot%\TEMP\*.*" /f /s /q
+for /D %%i in ("%SystemRoot%\TEMP\*") do RD /S /Q "%%i"
+
+
+RunDll32.exe InetCpl.cpl,ClearMyTracksByProcess 8
+erase "%LOCALAPPDATA%\Microsoft\Windows\Tempor~1\*.*" /f /s /q
+for /D %%i in ("%LOCALAPPDATA%\Microsoft\Windows\Tempor~1\*") do RD /S /Q "%%i"
